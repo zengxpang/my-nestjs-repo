350d772e2869bf2d7102305759a528e05c7b635f
zengxpang <zengxpang@163.com>
2023-08-21 18:07:14 +0800

    feat: :rocket: 增加typeOrm的例子

---
+++ apps/07_typeorm/src/user/entities/user.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({
+  name: 'aaa_user',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: 'aaa_name',
+    length: 50,
+  })
+  name: string;
+}
